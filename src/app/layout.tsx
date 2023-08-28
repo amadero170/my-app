635972ef0c40e246eef2b0a308de9d981cc948bf
amadero170 <amadero170@gmail.com>
2023-08-28 05:42:31 -0600

    otra

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Toyota Runner',
+  title: 'Toyota 4Runner',
   description: 'Toyota Runner 2005',
 }
 
